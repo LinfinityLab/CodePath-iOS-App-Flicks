1cff5082a67463edc07a7a1260aa47b8016aa46a
Weifan Lin <wlin15@citymail.cuny.edu>
2016-02-05 22:03:10 -0500

    Initial Commit

---
+++ MovieVeiwer/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  MovieVeiwer
+//
+//  Created by Weifan Lin on 2/5/16.
+//  Copyright © 2016 Weifan Lin. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
