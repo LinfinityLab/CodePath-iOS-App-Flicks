ac70f2a42c14947e6846a1a15dd46bc0099c7652
Weifan Lin <wlin15@citymail.cuny.edu>
2016-02-14 14:29:37 -0500

    add network err msg

--- MovieViewer/MoviesViewController.swift
+++ MovieViewer/MoviesViewController.swift
@@ -9,10 +9,12 @@
 import UIKit
 import AFNetworking
 import MBProgressHUD
+import SystemConfiguration
 
 class MoviesViewController: UIViewController, UITableViewDataSource, UITableViewDelegate {
 
     @IBOutlet weak var tableView: UITableView!
+    @IBOutlet weak var errMsgButton: UIButton!
     
     
     var movies: [NSDictionary]?
@@ -28,9 +30,34 @@ class MoviesViewController: UIViewController, UITableViewDataSource, UITableView
         refreshControl.addTarget(self, action: "refreshControlAction:", forControlEvents: UIControlEvents.ValueChanged)
         tableView.insertSubview(refreshControl, atIndex: 0)
         
-        loadDataFromNetwork()
+        if Reachability.isConnectedToNetwork() {
+            errMsgButton.hidden = true
+            loadDataFromNetwork()
 
+        } else {
+            errMsgButton.hidden = false
+        }
+        // Do any additional setup after loading the view.
     }
+    
+    internal class Reachability {
+        class func isConnectedToNetwork() -> Bool {
+            var zeroAddress = sockaddr_in()
+            zeroAddress.sin_len = UInt8(sizeofValue(zeroAddress))
+            zeroAddress.sin_family = sa_family_t(AF_INET)
+            let defaultRouteReachability = withUnsafePointer(&zeroAddress) {
+                SCNetworkReachabilityCreateWithAddress(nil, UnsafePointer($0))
+            }
+            var flags = SCNetworkReachabilityFlags()
+            if !SCNetworkReachabilityGetFlags(defaultRouteReachability!, &flags) {
+                return false
+            }
+            let isReachable = (flags.rawValue & UInt32(kSCNetworkFlagsReachable)) != 0
+            let needsConnection = (flags.rawValue & UInt32(kSCNetworkFlagsConnectionRequired)) != 0
+            return (isReachable && !needsConnection)
+        }
+    }
+    
     
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
